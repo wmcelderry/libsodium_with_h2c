6c35d067f17d8fa989939e24ed6738f4b674e0d1
W McElderry <8887746+wmcelderry@users.noreply.github.com>
2026-02-06 13:23:46 +0000

    Pull ristretto255 h2c functionality from libsodium:master - not my work!

--- src/libsodium/crypto_core/ed25519/core_ristretto255.c
+++ src/libsodium/crypto_core/ed25519/core_ristretto255.c
@@ -1,8 +1,12 @@
 
+#include <assert.h>
 #include <stdint.h>
+#include <string.h>
 
+#include "core_h2c.h"
 #include "crypto_core_ed25519.h"
 #include "crypto_core_ristretto255.h"
+#include "crypto_hash_sha256.h"
 #include "private/common.h"
 #include "private/ed25519_ref10.h"
 #include "randombytes.h"
@@ -65,6 +69,38 @@ crypto_core_ristretto255_from_hash(unsigned char *p, const unsigned char *r)
     ristretto255_from_hash(p, r);
 
     return 0;
+}
+
+static int
+_string_to_element(unsigned char *p,
+                   const char *ctx, const unsigned char *msg, size_t msg_len,
+                   int hash_alg)
+{
+    unsigned char h[crypto_core_ristretto255_HASHBYTES];
+
+    if (core_h2c_string_to_hash(h, sizeof h, ctx, msg, msg_len,
+                                hash_alg) != 0) {
+        return -1;
+    }
+    ristretto255_from_hash(p, h);
+
+    return 0;
+}
+
+int
+crypto_core_ristretto255_from_string(unsigned char p[crypto_core_ristretto255_BYTES],
+                                     const char *ctx, const unsigned char *msg,
+                                     size_t msg_len, int hash_alg)
+{
+    return _string_to_element(p, ctx, msg, msg_len, hash_alg);
+}
+
+int
+crypto_core_ristretto255_from_string_ro(unsigned char p[crypto_core_ristretto255_BYTES],
+                                        const char *ctx, const unsigned char *msg,
+                                        size_t msg_len, int hash_alg)
+{
+    return crypto_core_ristretto255_from_string(p, ctx, msg, msg_len, hash_alg);
 }
 
 void
